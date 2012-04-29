d383b985e1a7c1b9722074c26d97dbb0febacb0f
Yiran Sheng <me@yiransheng.com>
2012-04-29 13:54:39 -0400

    added quick multiple symbols support

--- server.js
+++ server.js
@@ -30,6 +30,7 @@
 var PORT = 4000;
 var FETCH_INTERVAL = 5000;
 var PRETTY_PRINT_JSON = true;
+var MAX_SIZE = 10;
 
 ///
 // START OF APPLICATION
@@ -41,19 +42,20 @@ var io = require('socket.io');
 var app = express();
 var server = http.createServer(app);
 var io = io.listen(server);
-io.set('log level', 1);
+io.set('log level', 2);
 
 server.listen(PORT);
 
-var ticker = "";
+var tickers = [];
 app.get('/:ticker/', function(req, res) {
-	ticker = req.params.ticker;
+	var ticker = req.params.ticker;
+	tickers = tickers.concat(ticker.split("-"));
 	res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket) {
-	var local_ticker = ticker;
-	ticker = "";
+	var local_ticker = tickers.splice(0, MAX_SIZE);
+	tickers.length = 0;
 
 	//Run the first time immediately
 	get_quote(socket, local_ticker);
@@ -68,39 +70,44 @@ io.sockets.on('connection', function(socket) {
 	});
 });
 
-function get_quote(p_socket, p_ticker) {
-	http.get({
-		host: 'www.google.com',
-		port: 80,
-		path: '/finance/info?client=ig&q=' + p_ticker
-	}, function(response) {
-		response.setEncoding('utf8');
-		var data = "";
+function get_quote(p_socket, p_tickers) {
+	var i;
+    for (i in p_tickers) {
+		p_ticker = p_tickers[i];
+		console.log(p_ticker);
+		http.get({
+			host: 'www.google.com',
+			port: 80,
+			path: '/finance/info?client=ig&q=' + p_ticker
+		}, function(response) {
+			response.setEncoding('utf8');
+			var data = "";
+						
+			response.on('data', function(chunk) {
+				data += chunk;
+			});
+			
+			response.on('end', function() {
+				if(data.length > 0) {
+					try {
+						var data_object = JSON.parse(data.substring(3));
+					} catch(e) {
+						return;
+					}
+										
+					var quote = {};
+					quote.ticker = data_object[0].t;
+					quote.exchange = data_object[0].e;
+					quote.price = data_object[0].l_cur;
+					quote.change = data_object[0].c;
+					quote.change_percent = data_object[0].cp;
+					quote.last_trade_time = data_object[0].lt;
+					quote.dividend = data_object[0].div;
+					quote.yield = data_object[0].yld;
 					
-		response.on('data', function(chunk) {
-			data += chunk;
-		});
-		
-		response.on('end', function() {
-			if(data.length > 0) {
-				try {
-					var data_object = JSON.parse(data.substring(3));
-				} catch(e) {
-					return;
+					p_socket.emit('quote', PRETTY_PRINT_JSON ? JSON.stringify(quote, true, '\t') : JSON.stringify(quote));
 				}
-									
-				var quote = {};
-				quote.ticker = data_object[0].t;
-				quote.exchange = data_object[0].e;
-				quote.price = data_object[0].l_cur;
-				quote.change = data_object[0].c;
-				quote.change_percent = data_object[0].cp;
-				quote.last_trade_time = data_object[0].lt;
-				quote.dividend = data_object[0].div;
-				quote.yield = data_object[0].yld;
-				
-				p_socket.emit('quote', PRETTY_PRINT_JSON ? JSON.stringify(quote, true, '\t') : JSON.stringify(quote));
-			}
+			});
 		});
-	});
+	}
 }
